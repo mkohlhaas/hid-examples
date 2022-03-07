de4d6bbd353d13dfb3fc528f06239c6b6fb504bd
Hans-J. Schmid <knock@myopendoor.de>
2022-03-07 19:30:14 +0100

    reformats souce code.

--- ch01/vocab3.hs
+++ ch01/vocab3.hs
@@ -1,45 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad (when)
 import Data.Char
-import Data.List (group, sort, sortBy)
+import Data.List (group, sort, sortOn)
 import Data.Ord
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import Control.Monad (when)
+import Fmt
 import System.Environment
 
-import Fmt
-
 type Entry = (Text, Int)
+
 type Vocabulary = [Entry]
 
 extractVocab :: Text -> Vocabulary
 extractVocab t = map buildEntry $ group $ sort ws
   where
-    ws = map T.toCaseFold $ filter (not . T.null)
-         $ map cleanWord $ T.words t
-    buildEntry xs@(x:_) = (x, length xs)
+    ws =
+      map T.toCaseFold $
+        filter (not . T.null) $
+          map cleanWord $ T.words t
+    buildEntry xs@(x : _) = (x, length xs)
     buildEntry [] = error "unexpected"
     cleanWord = T.dropAround (not . isLetter)
 
 allWords :: Vocabulary -> [Text]
-allWords vocab = map fst vocab
+allWords = map fst
 
 wordsCount :: Vocabulary -> (Int, Int)
 wordsCount vocab = (sum $ map snd vocab, length vocab)
 
 wordsByFrequency :: Vocabulary -> Vocabulary
-wordsByFrequency = sortBy (comparing $ Down . snd)
+wordsByFrequency = sortOn (Down . snd)
 
 allWordsReport :: Vocabulary -> Text
 allWordsReport vocab =
   fmt $ nameF "All words" $ unlinesF (allWords vocab)
 
 wordsCountReport :: Vocabulary -> Text
-wordsCountReport vocab = fmt $
-     "Total number of words: " +|total|+
-     "\nNumber of unique words: " +|unique|+ "\n"
+wordsCountReport vocab =
+  fmt $ "Total number of words: " +| total |+ "\nNumber of unique words: " +| unique |+ "\n"
   where
     (total, unique) = wordsCount vocab
 
@@ -47,18 +48,21 @@ wordsCountReport' :: Vocabulary -> Text
 wordsCountReport' vocab = T.unlines [part1, part2]
   where
     (total, unique) = wordsCount vocab
-    part1 = T.append (T.pack "Total number of words: ")
-                     (T.pack $ show total)
-    part2 = T.append (T.pack "Number of unique words: ")
-                     (T.pack $ show unique)
+    part1 =
+      T.append
+        (T.pack "Total number of words: ")
+        (T.pack $ show total)
+    part2 =
+      T.append
+        (T.pack "Number of unique words: ")
+        (T.pack $ show unique)
 
 frequentWordsReport :: Vocabulary -> Int -> Text
 frequentWordsReport vocab num =
-    fmt $ nameF "Frequent words"
-        $ blockListF' "" fmtEntry reportData
+  fmt $ nameF "Frequent words" $ blockListF' "" fmtEntry reportData
   where
     reportData = take num $ wordsByFrequency vocab
-    fmtEntry (t, n) = ""+|t|+": "+|n|+""
+    fmtEntry (t, n) = "" +| t |+ ": " +| n |+ ""
 
 processTextFile :: FilePath -> Bool -> Int -> IO ()
 processTextFile fname withAllWords n = do
@@ -72,8 +76,6 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["-a", fname, num] ->
-      processTextFile fname True (read num)
-    [fname, num] ->
-      processTextFile fname False (read num)
+    ["-a", fname, num] -> processTextFile fname True (read num)
+    [fname, num] -> processTextFile fname False (read num)
     _ -> putStrLn "Usage: vocab3 [-a] filename freq_words_num"
