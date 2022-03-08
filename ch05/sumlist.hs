af131adc5699c81085bf76c2a618b8b32c5ab8fd
Hans-J. Schmid <knock@myopendoor.de>
2022-03-08 09:17:00 +0100

    reduces eta.

--- ch05/sumlist.hs
+++ ch05/sumlist.hs
@@ -10,7 +10,7 @@ addItem' :: Integer -> State Integer ()
 addItem' n = modify' (+n)
 
 sumList :: [Integer] -> State Integer ()
-sumList xs = traverse_ addItem xs
+sumList = traverse_ addItem
 
 answer :: Integer
 answer = execState (sumList [1..100]) 0
